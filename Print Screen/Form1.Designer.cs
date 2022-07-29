457c9f8aa6ef6c27eb13ffdf3dfb5cab92d441e2
Islam <islamblal648@gmail.com>
2022-07-30 01:37:14 +0200

    Files Added

---
+++ Print Screen/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Print_Screen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnTakeScreenShot = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.picName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnTakeScreenShot
+            // 
+            this.btnTakeScreenShot.Location = new System.Drawing.Point(82, 90);
+            this.btnTakeScreenShot.Name = "btnTakeScreenShot";
+            this.btnTakeScreenShot.Size = new System.Drawing.Size(204, 32);
+            this.btnTakeScreenShot.TabIndex = 1;
+            this.btnTakeScreenShot.Text = "Take ScreenShot";
+            this.btnTakeScreenShot.UseVisualStyleBackColor = true;
+            this.btnTakeScreenShot.Click += new System.EventHandler(this.btnTakeScreenShot_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(134, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 30);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(186, 54);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(46, 30);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(238, 54);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(46, 30);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "0";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // picName
+            // 
+            this.picName.Location = new System.Drawing.Point(82, 12);
+            this.picName.Name = "picName";
+            this.picName.Size = new System.Drawing.Size(204, 30);
+            this.picName.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 125);
+            this.Controls.Add(this.picName);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnTakeScreenShot);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnTakeScreenShot;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox picName;
+    }
+}
+
---
+++ Print Screen/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Print_Screen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTakeScreenShot_Click(object sender, EventArgs e)
+        {
+            int left=int.Parse(textBox1.Text);
+            int top=int.Parse(textBox2.Text);
+            int right=int.Parse(textBox3.Text);
+            int bottom=int.Parse(textBox4.Text);
+
+            
+            if (picName.Text == "")
+            {
+                MessageBox.Show("Enter Pic Name");
+                picName.Focus();
+            }
+            else
+            {
+                int ynow = this.Top;
+                this.Top = Screen.PrimaryScreen.Bounds.Height + 1000;
+                Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Height, Screen.PrimaryScreen.Bounds.Width);
+                Graphics graph = Graphics.FromImage(bmp);
+                graph.CopyFromScreen(left, top, right, bottom, bmp.Size);
+                bmp.Save(picName.Text+".Jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                this.Top = ynow;
+
+                System.Diagnostics.Process.Start(picName.Text+".Jpeg");
+            }
+        }
+    }
+}
